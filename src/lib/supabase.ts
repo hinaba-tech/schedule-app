de4c50c3c9492564e731dd4011ea4e375f9aafc3
hinaba-tech <h_inaba@efly.co.jp>
2026-03-21 00:34:28 +0900

    Add src/lib/supabase.ts

---
+++ src/lib/supabase.ts
@@ -0,0 +1,17 @@
+import { createClient } from "@supabase/supabase-js";
+
+// サーバーサイド用クライアント
+export function createServerClient() {
+  return createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.SUPABASE_SERVICE_ROLE_KEY!
+  );
+}
+
+// クライアントサイド用クライアント
+export function createBrowserClient() {
+  return createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
+}
